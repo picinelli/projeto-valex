60cfe5686760320ff3faf1efff69d7735d701bc0
Pedro Ivo <cinelli.dev@gmail.com>
2022-07-11 11:19:30 -0300

    chore. heroku deploy config4.2

--- src/dist/utils/checkCardExpiration.js
+++
@@ -1,9 +0,0 @@
-export default function isCardExpired(date) {
-    var month = parseInt(date.split("/")[0]);
-    var year = parseInt(date.split("/")[1]) + 2000;
-    var expirationDate = new Date(year, month);
-    var now = new Date();
-    if (now >= expirationDate)
-        return true;
-    return false;
-}
--- src/dist/utils/schemaValidation.js
+++
@@ -1,9 +0,0 @@
-export default function validateSchema(schema, body) {
-    var _a = schema.validate(body), error = _a.error, value = _a.value;
-    if (error) {
-        throw {
-            type: 400,
-            message: error.details[0].message
-        };
-    }
-}
